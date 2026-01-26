1d7b6281ab745c1ca3e2e576417542a9a01478f6
Me <limesdk@mail.ru>
2026-01-27 00:25:49 +0300

    problem1

--- src/NotMain.java
+++ src/NotMain.java
@@ -1,12 +1,13 @@
 public class NotMain {
     public static void main(String[] args){
         int sum = 0;
-        for (int i = 1; i < 1000; i++)
-            if (i%3 == 0) {
-                sum = sum + i;
-            } else if (i%5 == 0) {
-                    sum = sum + i;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0) {
+                sum += i;
+            } else if (i % 5 == 0) {
+                sum += i;
             }
-        System.out.println(sum);
+        }
+        System.out.println(String.format("Ответ: %s",sum));
     }
 }
