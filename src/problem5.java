983a100ee28e8b86a4678df69158bcfa117ba7b3
Me <limesdk@mail.ru>
2026-01-28 19:42:06 +0300

    5problem

--- src/problem5.java
+++ src/problem5.java
@@ -1,14 +1,21 @@
+/* 2520 - самое маленькое число, которое делится без остатка на все числа от 1 до 10.
+Какое самое маленькое число делится нацело на все числа от 1 до 20? */
+
 public class problem5 {
     public static void main(String[] args) {
-        for (int number = 2; number <= 20; number++) {
-            for (int i = 2; i < number - 1; i++) {
-                if (number % i == 0) {
-
-                } else {
-                    System.out.println(number);
+        boolean isDivided = false;
+        int number = 1;
+        for (number = 1; !isDivided; number++) {
+            isDivided = true;
+            for (int i = 2; i <= 20; i++) {
+                if (number % i != 0) {  // если число не делится на i подбирается след число
+                    isDivided = false;
+                    break;
                 }
-
             }
+        }
+        if (isDivided) {
+            System.out.println(String.format("Искомое число %s", number - 1));
         }
     }
 }
