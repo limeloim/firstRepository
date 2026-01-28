b20a43d0cf8521b0ce224d1aa01cb9427975f95f
Me <limesdk@mail.ru>
2026-01-28 13:15:25 +0300

    9problem

--- src/problem9.java
+++ src/problem9.java
@@ -1,33 +1,17 @@
-//Тройка Пифагора - три натуральных числа a < b < c, для которых выполняется равенство
-//
-//a2 + b2 = c2
-//
-//Существует только одна тройка Пифагора, для которой a + b + c = 1000.
-//Найдите произведение abc.
 
 public class problem9 {
     public static void main(String[] args) {
         int a = 1;
         int b = 1;
         int c = 1;
-        int abc = 0;
-        for (; a <= 1000; a++) {
-            if (a * a + b * b == c * c && a + b + c == 1000) {
-                abc = a * b * c;
-            }
-            b = 1;
-            for (; b <= 1000; b++) {
-                if (a * a + b * b == c * c && a + b + c == 1000) {
-                    abc = a * b * c;
-                }
-                c = 1;
-                for (; c <= 1000; c++) {
-                    if (a * a + b * b == c * c && a + b + c == 1000) {
-                        abc = a * b * c;
-                    }
+        for (a = 1; a <= 1000; a++) {
+            for (b = a + 1; b  <= 1000; b++) {
+                c = 1000 - (a + b);
+                if (a*a + b*b == c*c) {
+                    int abc = a * b * c;
+                    System.out.println(String.format("произведение abc %s", abc));
                 }
             }
         }
-        System.out.println(String.format("произведение abc %s", abc));
     }
 }
