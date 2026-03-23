ddec39e0168d99404b41c559d34f01c713b3081a
Me <limesdk@mail.ru>
2026-03-23 16:53:02 +0300

    myProblem21

--- src/MyProblem.java
+++ src/MyProblem.java
@@ -1,167 +1,202 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 interface Feedable {
     public void receiveFeed();
 }
-interface openAccessExtension {
-    public void receiveAccessExtension();
+interface Accessable {
+
+    void openAccess();
+    void closeAccess();
 }
-interface ableToCastration {
+
+interface AbleToCastration {
     public void receiveCastration();
 }
 
+interface OpenAccessExtension {
+    public void receiveExtension();
+}
 
 public class MyProblem {
 
     public static void main(String[] args) {
-
+     
     Manager manager = new Manager();
     Scanner scanner = new Scanner(System.in);
-    SuperClass[] animals = new SuperClass[2];
+    ArrayList<Animal> animals = new ArrayList<>(2);
 
     while (true) {
-        if (isFull(animals)) {
-            animals = adSpace(animals);
-        }
-        for (int i = 0; i < animals.length; i++) {
-            if (animals[i] == null) {
-                System.out.printf(" %s[    ]", i + 1);
-            } else {
-                System.out.print(" " + (i + 1) + animals[i].getAllFilds());
-            }
-        }
-        System.out.println();
-        System.out.println("Введите команду: a = Create, b = Read, c = Update, d = Delete, e = Вызвать автоуход");
-        String command2 = scanner.nextLine();
-        switch (command2) {
-            case "a":
-                manager.create(animals, scanner);
-                break;
-            case "b":
-                manager.read(animals, scanner);
-                break;
-            case "c":
-                manager.update(animals, scanner);
-                break;
-            case "d":
-                manager.deleate(animals, scanner);
-                break;
-            case "e":
-                SuperClass.autoCare(animals);
-                break;
-        }
-    }
 
-
-}
-
-public static boolean isFull(SuperClass[] animals) {
-    for (SuperClass m : animals) {
-        if (m == null) return false;
-    }
-    return true;
-}
-
-public static SuperClass[] adSpace(SuperClass[] animals) {
-    SuperClass[] newArray = new SuperClass[animals.length * 2];
-    for (int i = 0; i < animals.length; i++) {
-        newArray[i] = animals[i];
-    }
-    return newArray;
-}
-}
-class Manager {
-
-    public void create(SuperClass[] animals, Scanner scanner) {
-            for (int i = 0; i < animals.length; i++) {
-                if (animals[i] == null) {
-                    System.out.println("Введите имя:");
-                    String name = scanner.nextLine();
-                    System.out.println("Введите возраст:");
-                    int age = scanner.nextInt();
-                    scanner.nextLine();
-                    System.out.println("Тип животного: a = Monkey, b = Lion, c = enguin, d = Cat");
-                    String command = scanner.nextLine();
-                    switch (command) {
-                        case "a":
-                            animals[i] = new Monkey(name, age);
-                            System.out.println("Создан Monkey");
-                            break;
-                        case "b":
-                            animals[i] = new Lion(name, age);
-                            System.out.println("Создан Lion");
-                            break;
-                        case "c":
-                            animals[i] = new Penguin(name, age);
-                            System.out.println("Создан Penguin");
-                            break;
-                        case "d":
-                            animals[i] = new Cat(name, age);
-                            System.out.println("Создан Cat");
-                            break;
-                    }
-                    return;
+        if (animals.isEmpty()) {
+            System.out.println("[Список пуст]");
+        } else {
+            for (Animal exactAnimal : animals) {
+                if (exactAnimal != null) {
+                    System.out.print(" [ " + exactAnimal.toString() + " ] ");
+                } else {
+                    System.out.print(" [ пусто ] ");
                 }
             }
         }
-
-
-        public void read(SuperClass[] animals,Scanner scanner) {
-
-            System.out.println("Введите номер животного");
-            int i = scanner.nextInt() - 1;
-            scanner.nextLine();
-            if (animals[i] == null) {
-                System.out.println("Нет");
-            } else {
-                System.out.println(animals[i].getAllFilds());
-            }
-        }
-
-        public void update(SuperClass[] animals,Scanner scanner) {
-            System.out.println("Введите номер Для редактирования");
-            int j = scanner.nextInt() - 1;
-            scanner.nextLine();
-            System.out.println("Введите комманду для редактирования: a = Имя, b = Возраст, c = Дата прибытия");
+            System.out.println();
+            System.out.println("Введите команду: a = Create, b = Read, c = Update, d = Delete, e = Вызвать автоуход");
             String command2 = scanner.nextLine();
             switch (command2) {
-                case "a": // Изменить имя
-                    System.out.println("Введите новое имя");
-                    String name = scanner.nextLine();
-                    animals[j].setName(name);
+                case "a":
+                    manager.create(animals, scanner);
                     break;
-                case "b": // Изменить возраст
-                    System.out.println("Введите новый Возраст");
-                    int age = scanner.nextInt();
-                    animals[j].setAge(age);
+                case "b":
+                    manager.read(animals, scanner);
                     break;
-                case "c": case "с":
-                    System.out.println("Введите новое время прибытия: yyyy [ENTER] mm [ENTER] dd [ENTER]");
-                    int yyyy = scanner.nextInt();
-                    int mm = scanner.nextInt();
-                    int dd = scanner.nextInt();
-                    animals[j].setTime(yyyy, mm, dd);
+                case "c":
+                    manager.update(animals, scanner);
+                    break;
+                case "d":
+                    manager.delete(animals, scanner);
+                    break;
+                case "e":
+                    Animal.autoCare(animals);
                     break;
             }
         }
+    }
+}
 
-        public void deleate(SuperClass[] animals,Scanner scanner) {
-            System.out.println("Выберите номер для удаления");
-            int k = scanner.nextInt() - 1;
-            scanner.nextLine();
-            animals[k] = null;
+class Manager {
+
+    public void create(ArrayList<Animal> animals, Scanner scanner) {
+        System.out.println("Введите имя:");
+        String name = scanner.nextLine();
+        System.out.println("Введите возраст:");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Тип животного: a = Monkey, b = Lion, c = Penguin, d = Cat");
+        String command = scanner.nextLine();
+        switch (command) {
+            case "a":
+                animals.add(new Monkey(name, age));
+                System.out.println("Создан Monkey");
+                break;
+            case "b":
+                Lion lion = new Lion(name, age, scanner);
+                System.out.println("Создан Lion");
+                System.out.println("болен Lion: Да Нет");
+                if (scanner.nextLine().equals("Да")) {
+                    lion.setIll(true);
+                } else {
+                    lion.setIll(false);
+                }
+                animals.add( lion);
+                break;
+            case "c":
+                animals.add( new Penguin(name, age, scanner));
+                System.out.println("Создан Penguin");
+                break;
+            case "d":
+                animals.add( new Cat(name, age));
+                System.out.println("Создан Cat");
+            break;
         }
     }
-class SuperClass { // Общие данные и методы животных должны быть вынесены в отдельный родительский класс (суперкласс).
-    
-    private String name;
-    private int age;
-    private LocalDate dateOfArrival;
-    private static Long countId = 1L;
-    private Long id;
 
-    public SuperClass(String name, int age) {
+    public void read(ArrayList<Animal> animals,Scanner scanner) {
+
+        System.out.println("Введите номер животного");
+        int i = scanner.nextInt() - 1;
+        scanner.nextLine();
+        System.out.println(animals.get(i).toString());
+    }
+
+    public void update(ArrayList<Animal> animals,Scanner scanner) {
+        System.out.println("Введите номер Для редактирования");
+        int j = scanner.nextInt() - 1;
+        scanner.nextLine();
+        System.out.println("Введите комманду для редактирования: a = Имя, b = Возраст, c = Дата прибытия, d = Обновить болезнь");
+        String command2 = scanner.nextLine();
+        switch (command2) {
+            case "a":
+                System.out.println("Введите новое имя");
+                String name = scanner.nextLine();
+                animals.get(j).setName(name);
+                break; 
+            case "b":
+                System.out.println("Введите новый Возраст");
+                int age = scanner.nextInt();
+                animals.get(j).setAge(age);
+                break; 
+            case "c":
+                System.out.println("Введите новое время прибытия: yyyy [ENTER] mm [ENTER] dd [ENTER]");
+                int yyyy = scanner.nextInt();
+                int mm = scanner.nextInt();
+                int dd = scanner.nextInt();
+                animals.get(j).setDateOfArrival(yyyy, mm, dd);
+                break;
+            case "d":
+                Animal animal = animals.get(j);
+                Lion lionLink = (Lion) animal;
+                System.out.println("Обновление болезни");
+                System.out.println("животное болеет: Да Нет");
+                if (scanner.nextLine().equals("Да")) {
+                    lionLink.setIll(true);
+                } else {
+                    lionLink.setIll(false);
+                }
+                break;
+
+        }
+    }
+
+    public void delete(ArrayList<Animal> animals, Scanner scanner) {
+        System.out.println("Выберите номер для удаления");
+        int k = scanner.nextInt() - 1;
+        scanner.nextLine();
+        animals.set(k, null);
+    }
+}
+    
+class Animal {
+
+    protected String name;
+    protected int age;
+    protected LocalDate dateOfArrival;
+    protected static Long countId = 1L;
+    protected Long id;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public LocalDate getDateOfArrival() {
+        return dateOfArrival;
+    }
+
+    public void setDateOfArrival(LocalDate dateOfArrival) {
+        this.dateOfArrival = dateOfArrival;
+    }
+
+    public static Long getCountId() {
+        return countId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public static void setCountId(Long countId) {
+        Animal.countId = countId;
+    }
+
+    public Animal(String name, int age) {
         this.name = name;
         this.age = age;
         this.id = countId++;
@@ -176,62 +211,119 @@ class SuperClass { // Общие данные и методы животных �
         this.age = age;
     }
 
-    public String getAllFilds() {
+    @Override
+    public String toString() {
         return String.format("[ Возраст:%d Имя:%s Дата прибытия:%s ID: %d]", this.age, this.name, this.dateOfArrival, this.id);
+
     }
 
-    void setTime(int yyyy, int mm, int dd) {
+    void setDateOfArrival(int yyyy, int mm, int dd) {
         this.dateOfArrival = LocalDate.of(yyyy, mm, dd);
     }
-    public static void autoCare(SuperClass[] animals) {
-        for (SuperClass animal : animals) {
+
+    public static void autoCare(ArrayList<Animal> animals) {
+        for (Animal animal : animals) {
             if (animal != null) {
                 animal.carCare();
             }
         }
     }
+
     public void carCare() {
 
     }
 
 }
 
-class Monkey extends SuperClass{
+class Monkey extends Animal{
     
     public Monkey(String name, int age) {
         super(name, age);
     }
 
     public void carCare() {
-        // вызов метода автоухода
+        //TODO: вызвать метод автоухода
     }
-
-
 }
 
-class Lion extends SuperClass implements openAccessExtension {
+class Lion extends Animal implements OpenAccessExtension {
 
-    boolean isIll;
+    private boolean isIll;
 
-    public Lion(String name, int age) {
+    private LocalDate dateOfBirth;
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setIll(boolean isIll) {
+        this.isIll = isIll;
+    }
+
+    public boolean getIll() {
+        return isIll;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[ Возраст:%d Имя:%s Дата прибытия:%s ID: %d Болеет:%s Деньрождения:%s]", this.age, this.name, this.dateOfArrival, this.id, this.isIll, this.dateOfBirth);
+
+    }
+
+    public Lion(String name, int age, Scanner scanner) {
+
         super(name, age);
+
+        System.out.println("Введите дату рождения (год [Enter] месяц [Enter] день [Enter]):");
+        int year = scanner.nextInt();
+        int month = scanner.nextInt();
+        int day = scanner.nextInt();
+        scanner.nextLine();
+
+        this.dateOfBirth = LocalDate.of(year, month, day);
     }
 
     public void carCare() {
-        receiveAccessExtension();
+        receiveExtension();
     }
 
-    public void receiveAccessExtension(){
-        // Открыть доступ льву
+    public void receiveExtension(){
+        //TODO: открыть доступ льву
+    }
+
+    public boolean isIll() {
+        return isIll;
     }
 }
 
-class Penguin extends SuperClass implements ableToCastration, Feedable {
+class Penguin extends Animal implements AbleToCastration, Feedable {
 
-    LocalDate dateOfBearth;
+    private LocalDate dateOfBirth;
 
-    public Penguin(String name, int age) {
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public Penguin(String name, int age, Scanner scanner) {
         super(name, age);
+
+        System.out.println("Введите дату рождения (год [Enter] месяц [Enter] день [Enter]):");
+        int year = scanner.nextInt();
+        int month = scanner.nextInt();
+        int day = scanner.nextInt();
+        scanner.nextLine(); // Очистка буфера после nextInt()
+
+        this.dateOfBirth = LocalDate.of(year, month, day);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[ Возраст:%d Имя:%s Дата прибытия:%s ID: %d Деньрождения:%s]", this.age, this.name, this.dateOfArrival, this.id, this.dateOfBirth);
+
     }
 
     public void carCare() {
@@ -240,14 +332,14 @@ class Penguin extends SuperClass implements ableToCastration, Feedable {
     }
 
     public void receiveCastration(){
-        //
+        //TODO: автоматизировать кострацию
     }
     public void receiveFeed(){
-        //
+        //TODO: автоматизировать корм
     }
 }
 
-class Cat extends SuperClass{
+class Cat extends Animal{
 
     public Cat(String name, int age) {
         super(name, age);
